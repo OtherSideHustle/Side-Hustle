add7a610d2d185efea2dbe888efd8cc1fa4d6289
Melissa Schwartz <melissjs@gmail.com>
2017-07-11 11:32:56 -0700

    new commit minus all the nodel stuff. Login for Miranda

--- index.js
+++ index.js
@@ -2,9 +2,26 @@ import React from 'react';
 import { render } from 'react-dom';
 import App from './components/App';
 import mapController from './controller/mapController'
+import { BrowserRouter as Router,  Route, Link } from 'react-router-dom'
+
+class Hello extends React.Component {
+  render(){return <div>Hello</div>}
+}
+
+class Bye extends React.Component {
+  render(){return <div>Bye</div>}
+}
 
 render(
-  <App />,
+  // <router><App /></router>,
+    <Router>
+      <div>
+      {/*<App />*/}
+      <Route path="/" component={App} />
+      {/*<Route path="/hello" component={Hello} />
+      <Route path="/bye" component={Bye} />*/}
+      </div>
+    </Router>,
   document.getElementById('root')
 );
 
